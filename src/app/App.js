c3bb0500c794540c4663dc37c824d3870c0689b7
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-12-02 17:22:12 +0700

    update

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ function App() {
     return <div className="bg-gray-700 min-h-screen">
         <Header />
         <div className="flex px-6 py-2 body-height">
-            <div className="w-0 lg:w-1/12 h-full pr-2">
+            <div className="hidden lg:block w-0 lg:w-1/12 h-full pr-2">
                 <Sidebar />
             </div>
             <div className="font-body w-full overflow-scroll lg:w-11/12">
