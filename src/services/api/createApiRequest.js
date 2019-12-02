8d3149d9879ba426b9ee6c5c5d1859c258a709dc
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-12-02 16:38:48 +0700

    update

--- src/services/api/createApiRequest.js
+++ src/services/api/createApiRequest.js
@@ -11,7 +11,26 @@ const createApiRequest = ({
         headers
     })
         .then(({ data }) => resolve(data))
-        .catch(err => reject(err.message))
+        .catch(err => {
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+            } else if (err.request) {
+                // The request was made but no response was received
+                // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(err.request);
+            } else {
+                // Something happened in setting up the request that triggered an err
+                console.log('err', err.message);
+            }
+            console.log(err.config);
+            if (err.response) return reject(err.response.data)
+            reject(err.message)
+        })
 })
 
 export default createApiRequest
