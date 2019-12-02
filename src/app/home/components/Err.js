af3353d974c08e29d2350d0c668cc80155342249
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-12-02 16:48:53 +0700

    Rollback HTTP upgrade

--- src/app/home/components/Err.js
+++ src/app/home/components/Err.js
@@ -5,6 +5,8 @@ const Err = props => {
 
     return !!err && <div className="max-w rounded overflow-hidden shadow-lg border border-8 border-blue-500 p-3 mb-4 text-red-500">
         {err}
+        <br />
+        Open Developer Tools for more informations.
     </div>
 }
 
